1b5183f24b951511b59f47d836daeec42f1c615e
afeiship <afeiship@gmail.com>
2019-01-26 09:11:18 +0800

    add: test compare length equal

--- test/test.js
+++ test/test.js
@@ -1,21 +1,21 @@
-(function () {
+(function() {
   var assert = require('assert');
   var nx = require('next-js-core2');
   var NxNumeric = require('../src/next-numeric');
 
-  describe('NxNumeric.methods', function () {
-    it('json/urlencoded', function () {
-      var data = {
-        key: 1,
-        value: 2
-      };
-      // console.log(rs1, rs2);
+  describe('NxNumeric.methods', function() {
+    test('NxNumeric.compare length is equal', function() {
+      var num1 = '1001';
+      var num2 = '1000';
+      var num3 = '1005';
+      var num4 = '1001';
+      var rs1 = NxNumeric.compare(num1, num2);
+      var rs2 = NxNumeric.compare(num1, num3);
+      var rs3 = NxNumeric.compare(num1, num4);
 
-      // assert.equal(rs1, '{"key":1,"value":2}');
-      // assert.equal(rs2, 'key=1&value=2');
-
+      expect(rs1).toBe(1);
+      expect(rs2).toBe(-1);
+      expect(rs3).toBe(0);
     });
   });
-
-
-}());
+})();
